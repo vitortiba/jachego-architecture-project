c4d42ed6fdee9b79b4e164e7c60d540cc42b006d
Vitor Tiba <vitor.hitiba@gmail.com>
2019-06-07 23:34:07 -0300

    adds repositories using interface

---
+++ Jachego.Infra/Repositories/CustomerRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jachego.Infra.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly StoreDataContext _context;
+
+        public CustomerRepository(StoreDataContext context)
+        {
+            _context = context;
+        }
+
+        public CustomerRepository GetById(Guid id)
+        {
+            return _context
+        }
+
+    }
+}
